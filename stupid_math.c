395e20d090d1a6218e5f86e2cafdc14ffc50124e
Elisa Angelinelli <angelinelli.elisa@gmail.com>
2024-02-22 09:37:58 +0100

    Added mean function and gitignore

---
+++ stupid_math.c
@@ -0,0 +1,5 @@
+#include "stupid_math.h" //includendo questo file ci portiamo dietro anche la libreria
+
+float mean (int a, int b) {
+    return (a + b) / 2;
+}
---
+++ stupid_math.h
@@ -0,0 +1,6 @@
+#ifndef _STUPIDMATHH_ //se qualcosa non è mai stato definito; ci protegge dalla ridefinizione; ci dice solo cosa è definito, ma non come; le funzioni sono definite nel file .c
+#define _STUPIDMATHH_
+
+float mean (int a, int b); //definiamo la funzione in base a ciò che prende in ingresso e ritorna in uscita
+
+#endif
